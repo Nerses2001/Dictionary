c9ab67c1eecdd0ad107a1e24efd6aea247622cac
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-01 10:46:59 -0800

    Impl Dictionary

---
+++ MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Drawing;
+
+namespace Dictionary
+{
+    internal class MyDictionary<TKey, TValue>
+    {
+        private readonly HashSet<TKey> _keys;
+        private readonly List<TValue> _values;
+
+        public MyDictionary()
+        {
+            _keys = new HashSet<TKey>();
+            _values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (ContainsKey(key))
+            {
+                throw new ArgumentException("An element with the same key already exists in the dictionary.");
+            }
+            _keys.Add(key);
+            _values.Add(value);
+        }
+        public bool ContainsKey(TKey key)
+        {
+            return _keys.Contains(key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (_keys.Remove(key))
+            {
+                int index = _values.FindIndex(value => key!.Equals(value));
+                if (index != -1)
+                {
+                    _values.RemoveAt(index);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Dictionary;
+
+MyDictionary<int, string> myDict = new ();
+
+myDict.Add(1, "One");
+myDict.Add(2, "Two");
+myDict.Add(3, "Three");
+
+Console.WriteLine("Does key 2 exist? " + myDict.ContainsKey(2));
+
+bool removed = myDict.Remove(2);
+Console.WriteLine("Key 2 removed? " + removed);
+
+Console.WriteLine("Does key 2 exist after removal? " + myDict.ContainsKey(2));
+
+Console.ReadLine(); 
